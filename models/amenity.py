8bc12067829cc386b39c1ab4aee11f87ebcda1a7
Nkulumba <kulumbanelson23@gmail.com>
2023-06-23 00:34:57 -0700

    changes to models

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
+""" State Module for the HBNB project """
 from os import getenv
-import sqlalchemy
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128),
-                      nullable=False)
+    """class for amenities"""
+    __tablename__ = 'amenities'
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
     else:
         name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
